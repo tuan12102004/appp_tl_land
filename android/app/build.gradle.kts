bcc83a7a4666d5f7c2c2ece6029a950993f49e34
Lê Đình Quý <105933653+ledinhquy123@users.noreply.github.com>
2025-11-07 17:13:03 +0700

    update ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.app_tl_land_3212"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.1.13356709"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
